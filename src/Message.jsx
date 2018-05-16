eb7b1cca1de9789c8c26bfe1a7aa2210e71bee6d
Aden Collinge <aden.collinge@gmail.com>
2018-05-16 22:19:05 +0000

    refactored message

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,26 +1,24 @@
 import React, {Component} from 'react';
 
-class Message extends Component {
-  render() {
+function Message ({content, username, color}) {
+
     const imgRegex = /(https?:\/\/.*\.(?:png|jpg|gif|jpeg))/ig;
-    let imgUrls = this.props.content.match(imgRegex);
-    let content = this.props.content.replace(imgRegex, "");
+    let imgUrls = content.match(imgRegex);
+    let contents = content.replace(imgRegex, "");
     let images = [];
     if (imgUrls) {
       images = imgUrls.map((url, index) => {
         return (<img className="message-image" src={url} key={index}/>)
       })
     }
-
     return (<div className="message">
       <span style={{
-          color: this.props.color
-        }} className="message-username">{this.props.username}</span>
-      <span className="message-content">{content}{"\n"}
+          color: color
+        }} className="message-username">{username}</span>
+      <span className="message-content">{contents}{"\n"}
         {images}
       </span>
     </div>);
-  }
 }
 
 export default Message;

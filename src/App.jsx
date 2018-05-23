755f4ae58f94483045f9dca04ab82e3847828aec
Aden Collinge <aden.collinge@gmail.com>
2018-05-23 13:22:42 +0000

    fixed bug that prevented app from updating state

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ class App extends Component {
             content: messageIn.content,
             system: true
           }
-          const updatedMessages = {...this.state.messages, newNotification} //this.state.messages.concat(newNotification)
+          const updatedMessages = this.state.messages.concat(newNotification)
           this.setState({messages: updatedMessages, usersOnline: messageIn.onlineUsers})
           break;
         default:
